70e6869b2ee02ecc503f047912de68ca8a5ba180
Jai Ganesh J <jjaiganeshjg95@gmail.com>
2018-06-17 18:14:07 +0530

    Update flappyBird.js

--- flappyBird.js
+++ flappyBird.js
@@ -75,7 +75,7 @@ function draw() {
     if (bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width
     && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= 
     pipe[i].y+constant) || bY+bird.height >= cvs.height - fg.height) {
-      window.location.reload(); //reload page
+      location.reload(); //reload page
       //window.location.href = window.location.href;
     }
     
